4c2a7e3645ea36e6db97f205afe3a7e981c435f8
Junaeid <md.junaeid.alam.se@gmail.com>
2026-06-12 19:46:36 -0400

    Created Database

---
+++ VehicleLink.Infrastructure/Migrations/20260612233108_InitialCreate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VehicleLink.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TelemetryLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VehicleId = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Latitude = table.Column<double>(type: "float", nullable: false),
+                    Longitude = table.Column<double>(type: "float", nullable: false),
+                    SpeedKmh = table.Column<double>(type: "float", nullable: false),
+                    EventType = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReceivedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TelemetryLogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TelemetryLogs_EventType",
+                table: "TelemetryLogs",
+                column: "EventType");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TelemetryLogs_Timestamp",
+                table: "TelemetryLogs",
+                column: "Timestamp");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TelemetryLogs_VehicleId",
+                table: "TelemetryLogs",
+                column: "VehicleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TelemetryLogs");
+        }
+    }
+}
